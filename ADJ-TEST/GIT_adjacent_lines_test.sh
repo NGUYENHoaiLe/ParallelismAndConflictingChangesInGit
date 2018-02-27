721b47600955a0688edf89e6f128200ccbc4cbe3
Hoai-Le.NGUYEN <hoai-le.nguyen@inria.fr>
2018-02-27 11:45:27 +0100

    add ADJ-TEST scriptsfor darcs and Git

---
+++ ADJ-TEST/GIT_adjacent_lines_test.sh
@@ -0,0 +1,57 @@
+# !/bin/bash
+
+
+TEST_NAME="TP1" # name of the working directory
+rm -rf ${TEST_NAME} # cleaning working directory
+mkdir -p ${TEST_NAME}
+cd ${TEST_NAME}
+
+# initialising initial git workspace
+mkdir ws1
+cd ws1
+git init 
+
+# adding a file to ws1
+# commit changes
+echo "1\n2\n3\n4\n5\n6\n" > file.txt
+git add file.txt
+git commit -m "ws1 | add 123456 to file.txt"
+cd ..
+
+# cloning three times ws1 (as ws2, ws3)
+git clone ws1 ws2
+git clone ws1 ws3
+
+
+
+# updating file.txt in ws2 (insert X at line 3, then write and quit 'ed')
+# commit changes
+cd ws2
+echo  "2d\nw\nq\n" | ed file.txt
+echo  "2i\nX\n.\nw\nq\n" | ed file.txt
+echo  "4d\nw\nq\n" | ed file.txt
+echo  "4i\nY\n.\nw\nq\n" | ed file.txt
+echo  "5d\nw\nq\n" | ed file.txt
+echo  "5i\nZ\n.\nw\nq\n" | ed file.txt
+git add file.txt
+git commit -m "ws2 | insert 12X34Y5Z"
+cd ..
+
+# updating file.txt in ws3 (insert Y at line 3, then write and quit 'ed')
+# commit changes
+cd ws3
+echo  "3d\nw\nq\n" | ed file.txt
+echo "3i\nW\n.\nw\nq\n" | ed  file.txt
+git add file.txt
+git commit -m "ws3 | insert 123W45"
+cd ..
+
+
+
+cd ws1
+git remote add refws2 ../ws2
+git remote add refws3 ../ws3
+git fetch refws2
+git fetch refws3
+git merge refws2/master refws3/master
+cd ..
