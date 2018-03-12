86b49d380a9ca5a04a564370ad9edc7f6f837edf
Hoai-Le.NGUYEN <hoai-le.nguyen@inria.fr>
2018-03-12 11:10:41 +0100

    update SVN_adjacent_lines_test.sh

--- ADJ-TEST/SVN_adjacent_lines_test.sh
+++ ADJ-TEST/SVN_adjacent_lines_test.sh
@@ -1,40 +1,62 @@
 # !/bin/bash
 
+###
+#svn need to be install on your local machine *Ubuntu 16.04*
+###
 
 TEST_NAME="TP1" # name of the working directory
 rm -rf ${TEST_NAME} # cleaning working directory
 mkdir -p ${TEST_NAME}
 cd ${TEST_NAME}
 
-# initialising initial git workspace
-mkdir ws1
-cd ws1
 
-# adding a file to ws1
-# commit changes
+
+
+svn mkdir trunk
+svn mkdir -m "Making a new dir." http://127.0.0.1/svn/myrepo/trunk
+svn mkdir branch
+svn mkdir -m "Making a new dir." http://127.0.0.1/svn/myrepo/branch
+
+mkdir trunk
+cd trunk
+svn co http://127.0.0.1/svn/myrepo/trunk --username admin
 echo "1\n2\n3\n4\n5\n6\n" > file.txt
-svn import file.txt file:///home/svn/myproject
+
+svn add file.txt
+svn commit -m "ws1 | add 123456 to file.txt"
+
+cd ..
 
 
 # create a branch
-svn copy file:///home/svn/myproject file:///home/svn/myproject/branch1 -m "Creating a private branch"
 
-#
-svn co file:///home/svn/myproject
+svn copy http://127.0.0.1/svn/myrepo/trunk http://127.0.0.1/svn/myrepo/branch -m "Creating a private branch"
+
+
+cd trunk
+svn co http://127.0.0.1/svn/myrepo/trunk --username admin
+
 echo  "2d\nw\nq\n" | ed file.txt
 echo  "2i\nX\n.\nw\nq\n" | ed file.txt
 echo  "4d\nw\nq\n" | ed file.txt
 echo  "4i\nY\n.\nw\nq\n" | ed file.txt
 echo  "5d\nw\nq\n" | ed file.txt
 echo  "5i\nZ\n.\nw\nq\n" | ed file.txt
-svn commit
+svn commit -m "ws2 | insert 12X34Y5Z"
+cd ..
 
-svn co file:///home/svn/myproject/branch1
+#
+mkdir branch
+cd branch
+svn switch
+svn co http://127.0.0.1/svn/myrepo/branch --username admin
+
 echo  "3d\nw\nq\n" | ed file.txt
 echo "3i\nW\n.\nw\nq\n" | ed  file.txt
-svn commit
+svn commit -m "ws3 | insert 123W45"
+cd ..
 
-
-svn co file:///home/svn/myproject
-svn merge  file:///home/svn/myproject/branch1
+cd trunk
+svn up	
+svn merge  http://127.0.0.1/svn/myrepo/branch
 
